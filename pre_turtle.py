288cc8bfcc12fb1503b0b079d44aaa1c45ffa151
simon <thesimontrapp@gmail.com>
2022-10-27 16:34:02 +0200

    Cleaned up pre_turtle

--- pre_turtle.py
+++ pre_turtle.py
@@ -4,7 +4,9 @@ import random
 # settings
 trt.setup(800, 800, 0, 0)
 trt.pensize(2)
-tt_pencolor = ['red', 'orange', 'yellow', 'purple', 'blue']
+tt1_pencolor = ['#FFBE86', '#FFE156', '#FFE9CE', '#FFB5C2', '#BD4F6C']
+tt2_pencolor = ['#ffbe0b', '#fb5607', '#ff006e', '#8338ec', '#3a86ff']
+tt3_pencolor = ['#9CFFFA', '#e6af2e', '#3d348b', '#885a5a', '#f6511d']
 trt.speed(200)
 trt.ht()
 x = 0
@@ -16,7 +18,7 @@ def turtle_movement2(angle, distance):
     while True:
         if a >= 360:
             break
-        trt.pencolor(random.choice(tt_pencolor))
+        trt.pencolor(random.choice(tt3_pencolor))
         trt.pendown()
         trt.fd(distance)
         trt.left(angle)
@@ -25,4 +27,4 @@ def turtle_movement2(angle, distance):
 
     trt.done()
 
-turtle_movement2(170, 600)
+turtle_movement2(145, 600)
